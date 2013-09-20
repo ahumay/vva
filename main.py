f56b1d3e19a09686dfae3d219d86f32e0f828891
Jesse Menn <jesse.menn@gmail.com>
2013-09-19 21:32:01 -0400

    find stress PRE/POST first/last stressed syl
    grab array of stresses, find pre, find post.

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ def main(filename):
 		procLine(line)
 	createStressArray(poem)
 
+#output stuff
 	for line in poem:
 		outstring = ""
 		for word in line['line']:
@@ -24,6 +25,49 @@ def main(filename):
 		for item in line['stressArray']:
 			outstring += '  ' + str(item) + ' '
 		print outstring
+
+# -- gok --
+	unstressPRE = 0
+	unstressPOST = 0
+	unstressTWEEN = 0
+	for line in poem:
+		print line['stressArray']
+		unstressPRE = findPRE(line['stressArray'])
+		unstressPOST = findPOST(line['stressArray'])
+		print "      ", "pre = ", unstressPRE, " post = ", unstressPOST
+
+
+def findTWEEN(line):
+	'''
+		"It also counts the number of unstressed syllables that occur 
+		between two consecutive stressed syllables throughout the poem.
+		-- M.R. Plamondon "Virtual Verse Analysis..." p132
+
+		So what if I got 0 1 0 1 0 1 0 1 0 1 0 1... which consecutive?
+		"
+
+		
+	'''
+
+def findPOST(line):
+	counter = 0 #init
+	for item in reversed(line):
+		if (item == 1):
+			break
+		elif (item == 0):
+			counter += 1
+	return counter
+
+def findPRE(line):
+	counter = 0 # init
+	for item in line:
+		if (item == 1):
+			break
+		elif (item == 0):
+			counter += 1
+	return counter
+
+
 
 ## main
 if __name__ == '__main__':
